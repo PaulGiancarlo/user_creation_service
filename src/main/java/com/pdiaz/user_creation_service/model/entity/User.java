58ec0d1e250a979a6a72c8de831942925bfb1e49
Paul Diaz <paulgiancarlod@gmail.com>
2025-04-11 15:05:05 -0400

    Adding script and model id

--- src/main/java/com/pdiaz/user_creation_service/model/entity/User.java
+++ src/main/java/com/pdiaz/user_creation_service/model/entity/User.java
@@ -1,12 +1,14 @@
 package com.pdiaz.user_creation_service.model.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "users")
 public class User {
     String name;
+    @Id
     String email;
     String password;
     String phones;
