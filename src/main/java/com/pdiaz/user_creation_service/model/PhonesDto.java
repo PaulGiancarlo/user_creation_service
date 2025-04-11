9d92f3309a894932265795f4226de69273a7d797
Paul Diaz <paulgiancarlod@gmail.com>
2025-04-11 11:55:39 -0400

    adding basic model

---
+++ src/main/java/com/pdiaz/user_creation_service/model/PhonesDto.java
@@ -0,0 +1,8 @@
+package com.pdiaz.user_creation_service.model;
+
+public class PhonesDto {
+    String number;
+    String cityCode;
+    String countryCode;
+
+}
---
+++ src/main/java/com/pdiaz/user_creation_service/model/UserDto.java
@@ -0,0 +1,10 @@
+package com.pdiaz.user_creation_service.model;
+
+import java.util.List;
+
+public class UserDto {
+    String name;
+    String email;
+    String password;
+    List<PhonesDto> phones;
+}
