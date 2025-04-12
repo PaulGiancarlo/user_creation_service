021d68fa8d06d43e42859c972e0016c68943049d
Paul Diaz <paulgiancarlod@gmail.com>
2025-04-12 09:36:18 -0400

    adding validations

--- src/main/java/com/pdiaz/user_creation_service/model/PhonesDto.java
+++ src/main/java/com/pdiaz/user_creation_service/model/PhonesDto.java
@@ -1,8 +1,17 @@
 package com.pdiaz.user_creation_service.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
 public class PhonesDto {
-    String number;
-    String cityCode;
-    String countryCode;
+    @NotBlank(message = "Please add a valid number")
+    @Size(min=2, max= 9, message = "Please add a valid phone number")
+    private String number;
+    @NotBlank(message = "Please add a City Code")
+    private String cityCode;
+    @NotBlank(message = "Please add a Country Code")
+    private String countryCode;
 
 }
--- src/main/java/com/pdiaz/user_creation_service/model/UserDto.java
+++ src/main/java/com/pdiaz/user_creation_service/model/UserDto.java
@@ -1,10 +1,24 @@
 package com.pdiaz.user_creation_service.model;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Data;
 import java.util.List;
 
+
+@Data
 public class UserDto {
-    String name;
-    String email;
-    String password;
-    List<PhonesDto> phones;
+    @NotBlank(message= "Please provide a name")
+    private String name;
+
+    @Email(message = "The email is not valid")
+    @NotBlank(message = "Please provide a email")
+    private String email;
+
+    @NotBlank(message = "Please provide a password")
+    private String password;
+
+    @NotEmpty(message = "Please provide at least one telephone number")
+    private List<PhonesDto> phones;
 }
