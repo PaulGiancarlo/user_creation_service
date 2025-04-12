9db9a48783124fe34239ed1afdae0f1e9ef23a6b
Paul Diaz <paulgiancarlod@gmail.com>
2025-04-12 09:35:34 -0400

    utils

---
+++ src/main/java/com/pdiaz/user_creation_service/util/JwtUtil.java
@@ -0,0 +1,23 @@
+package com.pdiaz.user_creation_service.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+import java.security.Key;
+import java.util.Date;
+
+public class JwtUtil {
+    private static final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+    public static String generateToken(String userEmail){
+        return Jwts.builder()
+                .setSubject(userEmail)
+                .claim("email", userEmail)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 3600000))
+                .signWith(key)
+                .compact();
+
+    }
+}
---
+++ src/main/java/com/pdiaz/user_creation_service/util/UserMapper.java
@@ -0,0 +1,32 @@
+package com.pdiaz.user_creation_service.util;
+
+import com.pdiaz.user_creation_service.model.UserDto;
+import com.pdiaz.user_creation_service.model.entity.Phones;
+import com.pdiaz.user_creation_service.model.entity.User;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class UserMapper {
+    public static User mapToEntity(UserDto userDto, String token){
+        User user = new User();
+        user.setEmail(userDto.getEmail());
+        user.setName(userDto.getName());
+        user.setPassword(userDto.getPassword());
+
+        if(userDto.getPhones()!=null) {
+            List<Phones> phones = userDto.getPhones().stream().map(phoneDto -> {
+                Phones phone = new Phones();
+                phone.setNumber(phoneDto.getNumber());
+                phone.setCityCode(phoneDto.getCityCode());
+                phone.setCountryCode(phoneDto.getCountryCode());
+                phone.setUser(user);
+                return phone;
+            }).collect(Collectors.toList());
+
+            user.setPhones(phones);
+            user.setToken(token);
+        }
+        return user;
+    }
+}
